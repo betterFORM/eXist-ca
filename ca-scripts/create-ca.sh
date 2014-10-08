2bcff06baf05b62d0d7c9fee2e5f54ff6ba87f4f
Olaf Schreck <chakl@syscall.de>
2014-10-08 20:56:15 +0200

    barebones shell script and some doc

--- ca-scripts/create-ca.sh
+++ ca-scripts/create-ca.sh
@@ -1,4 +1,27 @@
 #!/bin/sh 
-echo hello world  >/tmp/junktest
-pwd >>/tmp/junktest
-echo $* >>/tmp/junktest
+
+# just some info gathering (to understand paths, env etc)
+# this cruft goes away when I understand tow to call scripts correctly
+echo "pwd:"
+pwd
+echo "environment:"
+env
+echo "cmdline:"
+echo $*
+
+
+TESTRUN=1         # will delete previously generated CA data
+
+EASYRSA_HOME=/some/where/easyrsa2
+CERT_DIR=$EASYRSA_HOME/keys
+
+
+# this script assumes $EASYRSA_HOME/vars has been created correctly with 
+# parameters from XForms.  It does NOT create $EASYRSA_HOME/vars.
+
+# traditional steps to create in initial CA with easyrsa
+#cd $EASYRSA_HOME
+#. ./vars
+#./clean-all
+#./build-dh
+#./pkitool --initca --pass
