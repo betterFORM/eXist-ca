c40c92a4e779e4e661994ba0f02b99eaa8a337cb
Chakl <ckl.github@syscall.de>
2014-12-16 19:37:27 +0100

    new ca-scripts/create-casrvcert.sh, factored out from create-ca.sh

---
+++ ca-scripts/create-casrvcert.sh
@@ -0,0 +1,209 @@
+#!/bin/sh 
+
+# create a web server certificate for eXist itself in one go (req,sign,pkcs12)
+# this functionality used to be in create-ca.sh before
+#
+# The following environment vars need to be passed from the caller 
+# (vars may have no value, but must be present in the environment):
+#   EXISTCA_CAPASS        (eg "craps")
+#   EXISTCA_CANAME        (eg "Example CA")
+#   EXISTCA_CERTNAME      (eg "John Doe", "existca.example.org")
+#   EXISTCA_CERTPASS      (eg "test")
+#   EXISTCA_CERTTYPE      (eg "client", "server")
+#   EXISTCA_CERTKEYSIZE   (eg 2048)
+#   EXISTCA_CERTEXPITE    (eg 1825)
+#   EXISTCA_CERTCOUNTRY   (eg "DE")
+#   EXISTCA_CERTPROVINCE  (eg "Berlin")
+#   EXISTCA_CERTCITY      (eg "Berlin")
+#   EXISTCA_CERTORG       (eg "Example Org")
+#   EXISTCA_CERTOU        (eg "CA")
+#   EXISTCA_CERTEMAIL     (eg "ca@example.org")
+#   EXISTCA_HOME          (eg /usr/local/eXistCA)
+#   EXISTCA XMLOUT
+#   PKI_BASE
+#
+# This script returns the following exit codes:
+#   0  - success
+#   1  - fail: parameter problem
+#   2  - fail: cert creation
+
+
+# required env vars as documented above
+REQ_ENV="\
+ EXISTCA_CAPASS \
+ EXISTCA_CANAME \
+ EXISTCA_CERTNAME \
+ EXISTCA_CERTPASS \
+ EXISTCA_CERTTYPE \
+ EXISTCA_CERTKEYSIZE \
+ EXISTCA_CERTEXPIRE \
+ EXISTCA_CERTCOUNTRY \
+ EXISTCA_CERTPROVINCE \
+ EXISTCA_CERTCITY \
+ EXISTCA_CERTORG \
+ EXISTCA_CERTOU \
+ EXISTCA_CERTEMAIL \
+ EXISTCA_HOME \
+ EXISTCA_EXPORTPASS \
+ EXISTCA_XMLOUT \
+ PKI_BASE \
+"
+
+#FAKE="echo"
+#DEBUG=1
+if [ -n "$DEBUG" ]; then
+    echo -n "cmdline: "
+    echo $*
+    echo -n "pwd: "
+    pwd
+    echo "environment:"
+    env
+    echo "stdin:"
+    while read line; do echo $line; done
+fi
+
+# dump server data as XML
+dump_xml () {
+    srv_crt=`cat $EASYRSA_PKI/issued/${THIS_SRV}.crt`
+    srv_key=`cat $EASYRSA_PKI/private/${THIS_SRV}.key`
+    #srv_pkcs12=`cat $EASYRSA_PKI/private/${THIS_SRV}.p12`
+    srv_pkcs12='this is binary and needs encoding'
+    srv_req=`cat $EASYRSA_PKI/reqs/${THIS_SRV}.req`
+    ca_serial=`tr -dc "[:xdigit:]" <$EASYRSA_PKI/serial`
+    read status expire serial unkn cn <<INDEX
+$(grep "$THIS_SRV" $EASYRSA_PKI/index.txt)
+INDEX
+#    exp_date=`openssl x509 -text -in $EASYRSA_PKI/issued/${THIS_SRV}.crt | grep "Not After" | sed 's/.*Not After : //;`
+    printf "
+<cert name=\"$THIS_SRV\" nicename=\"$EXISTCA_CERTNAME\">
+  <certtype>server</certtype>
+  <serial>$serial</serial>
+  <status>$status</status>
+  <expire-timestamp>$expire</expire-timestamp>
+  <certpass>$EXISTCA_CERTPASS</certpass>
+  <country>$EXISTCA_CERTCOUNTRY</country>
+  <province>$EXISTCA_CERTPROVINCE</province>
+  <city>$EXISTCA_CERTCITY</city>
+  <org>$EXISTCA_CERTORG</org>
+  <org-unit>$EXISTCA_CERTOU</org-unit>
+  <email>$EXISTCA_CERTEMAIL</email>
+  <cert>$srv_crt</cert>
+  <key>$srv_key</key>
+  <pkcs12>$srv_pkcs12</pkcs12>
+  <req>$srv_req</req>
+</cert>
+" >$EXISTCA_XMLOUT
+}
+
+# source common script vars
+. $EXISTCA_HOME/script-vars.sh
+
+err=0
+
+# env sanity checks
+if ! checkenv $REQ_ENV; then
+    logmsg "ERROR - refuse to work on incomplete data"
+    err=1
+fi
+
+### validate user provided input data
+
+# verify keysize user input
+if ! verify_rsakeysize $EXISTCA_CERTKEYSIZE; then
+    logmsg "ERROR - key size $EXISTCA_CERTKEYSIZE not supported"
+    err=1
+fi
+# verify expire value is postive integer
+if ! verify_posint $EXISTCA_CERTEXPIRE; then
+    logmsg "ERROR - invalid $EXISTCA_CERTEXPIRE data"
+    err=1
+fi
+
+# cleanup obscure chars out of passed CA name, for use as file name
+THIS_CA=`echo -n "$EXISTCA_CANAME" | tr -cd '[:alnum:]'`
+
+# cleanup obscure chars out of passed Common Name, for use as file name
+THIS_SRV=`echo -n "$EXISTCA_CERTNAME" | tr -cd '[:alnum:].-'`
+
+# err out with exit code 1 (parameter problem)
+if [ $err -ne 0 ]; then
+    logmsg "ERROR creating web server cert: parameter problem"
+    printf "<cert/>"
+    exit 1
+fi
+
+
+### setup env for easyrsa
+
+# define EASYRSA_PKI to point to $THIS_CA directory
+export EASYRSA_PKI=${PKI_BASE}/${THIS_CA}
+
+export EASYRSA_KEY_SIZE=$EXISTCA_CERTKEYSIZE
+export EASYRSA_CERT_EXPIRE=$EXISTCA_CERTEXPIRE
+export EASYRSA_REQ_CN=$THIS_SRV
+export EASYRSA_REQ_COUNTRY=$EXISTCA_CERTCOUNTRY
+export EASYRSA_REQ_PROVINCE=$EXISTCA_CERTPROVINCE
+export EASYRSA_REQ_CITY=$EXISTCA_CERTCITY
+export EASYRSA_REQ_ORG=$EXISTCA_CERTORG
+export EASYRSA_REQ_OU=$EXISTCA_CERTOU
+export EASYRSA_REQ_EMAIL=$EXISTCA_CERTEMAIL
+
+# reset and export auth related vars
+EXISTCA_AUTHIN=
+EXISTCA_AUTHOUT=
+EXISTCA_AUTHPASS=
+export EXISTCA_AUTHIN EXISTCA_AUTHOUT EXISTCA_AUTHPASS
+export EXISTCA_CAPASS EXISTCA_CERTPASS EXISTCA_EXPORTPASS
+
+cd $EASYRSA
+
+# create web server cert request
+if [ -n "$EXISTCA_CERTPASS" ]; then
+    EXISTCA_AUTHOUT="env:EXISTCA_CERTPASS"
+else
+    EXISTCA_AUTHOUT=
+    genreq_opt=nopass
+fi
+EXISTCA_AUTHIN=
+$FAKE sh ./easyrsa gen-req "$EASYRSA_REQ_CN" $genreq_opt
+if [ $? -ne 0 ]; then
+    logmsg "ERROR \"$THIS_CA\" - failed to generate web server certificate request"
+    err=1
+fi
+
+# sign web server cert request
+EXISTCA_AUTHIN="env:EXISTCA_CAPASS"
+EXISTCA_AUTHOUT=
+$FAKE sh ./easyrsa sign-req server "$EASYRSA_REQ_CN"
+if [ $? -ne 0 ]; then
+    logmsg "ERROR \"$THIS_CA\" - failed to sign web server certificate"
+    err=1
+fi
+
+# build PKCS12 cert data structure
+if [ -n "$EXISTCA_CERTPASS" ]; then
+    EXISTCA_AUTHIN="env:EXISTCA_CERTPASS"
+    #EXISTCA_AUTHPASS="env:EXISTCA_CERTPASS"
+    EXISTCA_AUTHPASS="env:EXISTCA_EXPORTPASS"
+else
+    EXISTCA_AUTHIN=
+    EXISTCA_AUTHPASS="env:EXISTCA_EXPORTPASS"
+fi
+$FAKE sh ./easyrsa export-p12 "$EASYRSA_REQ_CN"
+if [ $? -ne 0 ]; then
+    logmsg "ERROR \"$THIS_CA\" - failed to pkcs12 export web server certificate"
+    err=1
+fi
+
+
+# dump XML data to stdout regardless of possible errors
+dump_xml
+
+# err out with exit code 3 (server cert)
+if [ $err -ne 0 ]; then
+    logmsg "ERROR \"$THIS_CA\" - failed to create web server certificate"
+    exit 2
+fi
+
+
+exit 0
