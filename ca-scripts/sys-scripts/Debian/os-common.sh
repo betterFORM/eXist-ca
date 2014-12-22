c5e49df5aa6261e1b1104395f776e2dc5506b409
Chakl <ckl.github@syscall.de>
2014-12-22 19:37:26 +0100

    implement get_if_dhcp() and reconfig_if() for Debian, set_if_dhcp() pending

--- ca-scripts/sys-scripts/Debian/os-common.sh
+++ ca-scripts/sys-scripts/Debian/os-common.sh
@@ -1,24 +1,29 @@
 #!/bin/sh 
 
+
+INTERFACES=/etc/network/interfaces
+
+
 # check if interface is already configured for DHCP
 get_if_dhcp () {
     if=$1
-    return 1
+    grep -q "iface[[:space:]]*${if}.*dhcp" $INTERFACES
 }
 
 # create interface configuration for DHCP
 set_if_dhcp () {
     if=$1
+    # better leave that to a perl script, way too complicated to do in shell..
     return 1
 }
 
 # change interface configuration
 reconfig_if () {
     if=$1
-    return 1
+    ifup $if
 }
 
-# rebuild ntpd.conf and restart ntp daemon
+# rebuild ntpd.conf
 reconfig_ntpd () {
     srvs=$*
     file=/etc/ntp.conf
@@ -30,7 +35,10 @@ reconfig_ntpd () {
 	SEDOUT="server $s\n"
     done
     sed -e "s/%SERVERDEFS%/$SEDOUT/;" <Debian/sample-ntp.conf >$file
+}
 
+# restart ntp daemon
+restart_ntpd () {
     # explicitly kill and start rather than "/etc/rc.d/ntpd restart"
     pkill ntpd
     if pgrep -lf ntpd >/dev/null; then
