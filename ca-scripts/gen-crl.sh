d70dc9bbbfec48ce64dae1b377f9a85779825493
Chakl <ckl.github@syscall.de>
2014-12-24 00:40:19 +0100

    update to common standards

--- ca-scripts/gen-crl.sh
+++ ca-scripts/gen-crl.sh
@@ -5,58 +5,82 @@
 # The following environment vars need to be passed from the caller:
 #   EXISTCA_CAPASS      (eg "craps")
 #   EXISTCA_CANAME      (eg "Example CA")
+#   EXISTCA_HOME        (eg /usr/local/eXistCA)
+#   EXISTCA XMLOUT
+#   PKI_BASE
+#
+# This script returns the following exit codes:
+#   0  - success
+#   1  - fail: parameter problem
+#   2  - fail: CRL generation
+
 
 # required env vars as documented above
 REQ_ENV="\
  EXISTCA_CAPASS \
  EXISTCA_CANAME \
  EXISTCA_HOME \
+ EXISTCA_XMLOUT \
  PKI_BASE \
 "
 
 #FAKE="echo"
-#DEBUG=1
-if [ -n "$DEBUG" ]; then
-    echo "cmdline: "
-    echo $*
-    echo "pwd: "
-    pwd
-    echo "environment:"
-    env
-    echo "stdin:"
-    while read line; do echo $line; done
-fi
 
+# dump CA and server data as XML
+dump_xml () {
+    printf "
+<crl/>
+" >$EXISTCA_XMLOUT
+}
 
 # source common script vars
 . $EXISTCA_HOME/script-vars.sh
 
+err=0
+
 # env sanity checks
 if ! checkenv $REQ_ENV; then
     echo "ERROR - refuse to work on incomplete data"
-    exit 1
+    err=1
 fi
 
-# XXX validate all user provided input data!
-
-err=0
+### validate user provided input data
 
 # cleanup obscure chars out of passed CA name, for use as file name
 THIS_CA=`echo "$EXISTCA_CANAME" | tr -cd '[:alnum:]'`
 
+# err out with exit code 1 (parameter problem)
+if [ $err -ne 0 ]; then
+    logmsg "ERROR \"$THIS_CA\" - generating CRL: parameter problem"
+    printf "<crl/>"
+    exit 1
+fi
+
+
+### setup env for easyrsa
+
 # define EASYRSA_PKI to point to $THIS_CA directory
 export EASYRSA_PKI=${PKI_BASE}/${THIS_CA}
+
+# reset and export auth related vars
+EXISTCA_AUTHIN="env:EXISTCA_CAPASS"
+EXISTCA_AUTHOUT=
+export EXISTCA_AUTHIN EXISTCA_AUTHOUT EXISTCA_CAPASS
 
 cd $EASYRSA
 
 # create crl
-EXISTCA_AUTHIN="env:EXISTCA_CAPASS"
-export EXISTCA_AUTHIN EXISTCA_CAPASS
 $FAKE ./easyrsa gen-crl
 if [ $? -ne 0 ]; then
-    echo "ERROR creating certificate revocation list"
-    exit 1
+    logmsg "ERROR creating certificate revocation list"
+    printf "<CA/>"
+    exit 2
 fi
+
+# dump XML data to stdout regardless of possible errors (some XML fields 
+# may be left blank if server cert failed)
+dump_xml
+
 
 exit 0
 
